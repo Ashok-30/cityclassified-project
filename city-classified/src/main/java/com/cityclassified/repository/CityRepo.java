86365276ab890fef2b4e43a0557f593ae860d81d
Ashok-30 <83399552+Ashok-30@users.noreply.github.com>
2021-06-22 12:28:55 +0530

    Delete city-classified directory

--- city-classified/src/main/java/com/cityclassified/repository/CityRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cityclassified.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cityclassified.model.City;
-
-public interface CityRepo extends JpaRepository<City, Integer> {
-
-}
